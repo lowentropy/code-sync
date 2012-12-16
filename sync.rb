ac53ed56cf521a34c78f0325f0f9025f04f81db8
Nathan Matthews <lowentropy@gmail.com>
2012-12-16 12:17:33 -0600

    Fix bug, whitespace in remote path

--- sync.rb
+++ sync.rb
@@ -12,7 +12,7 @@ usage unless (idx = ARGV[1].index ':')
 
 local = File.realdirpath File.expand_path ARGV[0]
 host, path = ARGV[1][0,idx], ARGV[1][idx+1..-1]
-remote = "#{host}:#{`ssh #{host} 'readlink #{path}'`}"
+remote = "#{host}:#{`ssh #{host} 'readlink #{path}'`}".strip
 ignore = [%r{tmp/}, /\.(ya?ml|sql|log|tmp)$/]
 
 puts "Sync: #{local} -> #{remote}"
