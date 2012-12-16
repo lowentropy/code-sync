99bbf5a108d06813f1f686f63983acbd6b82222b
Nathan Matthews <lowentropy@gmail.com>
2012-12-16 13:01:10 -0600

    Make sure remote directories are created

--- sync.rb
+++ sync.rb
@@ -12,16 +12,18 @@ usage unless (idx = ARGV[1].index ':')
 
 local = File.realdirpath File.expand_path ARGV[0]
 host, path = ARGV[1][0,idx], ARGV[1][idx+1..-1]
-remote = "#{host}:#{`ssh #{host} 'readlink #{path}'`}".strip
+remote = `ssh #{host} 'readlink #{path}'`.strip
 ignore = [%r{tmp/}, /\.(ya?ml|sql|log|tmp)$/]
 
-puts "Sync: #{local} -> #{remote}"
+puts "Sync: #{local} -> #{host}:#{remote}"
 
 Listen.to(local, ignore: ignore, relative_paths: true) do |modified, added, removed|
   (modified + added + removed).each do |path|
     source = File.join local, path
     target = File.join remote, path
-    `rsync -rpvzq #{source} #{target}`
+    target_dir = File.dirname target
+    `ssh #{host} 'mkdir -p #{target_dir}'`
+    `rsync -rpvzq #{source} #{host}:#{target}`
     puts path
   end
 end
